8c295fa149582b69fcd1fe35a06707d983c646f1
Tomáš Chaloupka <chalucha@gmail.com>
2015-07-31 09:12:32 +0200

    wip RSA with botan

--- source/jwtd/jwt_botan.d
+++ source/jwtd/jwt_botan.d
@@ -21,6 +21,21 @@ version (UseBotan) {
 			sign = hmac.finished()[].dup;
 		}
 
+//		void sign_rs(ubyte* hash, int type, uint len, uint signLen) {
+//			sign = new ubyte[len];
+//
+//			RSA* rsa_private = RSA_new();
+//			BIO* bpo = BIO_new_mem_buf(cast(char*)key.ptr, -1);
+//			if(bpo is null)
+//				throw new Exception("Can't load the key.");
+//			PEM_read_bio_RSAPrivateKey(bpo, &rsa_private, null, null);
+//			BIO_free(bpo);
+//			if(rsa_private is null)
+//				throw new Exception("Can't create RSA key.");
+//			RSA_sign(type, hash, signLen, sign.ptr, &signLen, rsa_private);
+//			RSA_free(rsa_private);
+//		}
+
 		switch(algo) {
 			case JWTAlgorithm.NONE: {
 				break;
@@ -41,6 +56,21 @@ version (UseBotan) {
 				break;
 			}
 			case JWTAlgorithm.RS256:
+				import botan.pubkey.algo.rsa;
+				import botan.rng.auto_rng;
+				import botan.filters.data_src;
+                
+                Unique!SHA256 hash = new SHA256();
+                hash.update(msg);
+
+				Unique!AutoSeededRNG rng = new AutoSeededRNG;
+				auto privKey = loadKey(cast(DataSource)DataSourceMemory(key), *rng);
+                auto signer = PKSigner(privKey, "EMSA4(SHA-256)");
+                auto res = signer.signMessage(cast(const(ubyte)*)msg.ptr, msg.length, *rng);
+                //auto res = signer.signMessage(hash.finished(), *rng);
+                sign = res[];
+				//sign_rs(hash.ptr, NID_sha256, 256, SHA256_DIGEST_LENGTH);
+				break;
 			case JWTAlgorithm.RS384:
 			case JWTAlgorithm.RS512:
 			case JWTAlgorithm.ES256:
